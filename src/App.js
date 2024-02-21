770d4323d417dbe439ec1cb7860537a8fa45ea3a
Emilio Van Der Linden <emilio.vanderlinden@rebin.be>
2024-02-21 12:24:55 +0100

    Create contact page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      rebin
-    </div>
+    <>
+      <Contact></Contact>
+    </>
   );
 }
 
