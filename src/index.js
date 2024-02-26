1bd9164b6e3c81fbcaad312e85717e9444031275
Senne Malfliet <senne.malfliet@rebin.be>
2024-02-26 12:21:28 +0100

    remove chakra-ui (unused library)

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
