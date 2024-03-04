c4b7500a7b8b39c84beb2dc1e751b86663d2cd77
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-04 14:12:52 +0100

    change size of video

--- src/components/AboutVideo.js
+++ src/components/AboutVideo.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const AboutVideo = () => {
   return (
-    <div className="place-self-center md:max-w-xl md:pl-6 md:py-6 my-auto">
-      <video className="rounded-lg lg:w-11/12" controls>
+    <div className="place-self-center md:pl-6 md:py-6 my-auto">
+      <video className="rounded-lg max-w-xl" controls>
         <source
           src="https://docs.material-tailwind.com/demo.mp4"
           type="video/mp4"
