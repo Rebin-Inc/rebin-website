224b35ee3489f830f7feabb094220b9ff29fb2e4
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-11 14:17:07 +0100

    make responsive

--- src/pages/home/components/Testimonial.js
+++ src/pages/home/components/Testimonial.js
@@ -11,16 +11,14 @@ const Testimonial = () => {
     "Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.";
 
   return (
-    <section className="mt-10 grid grid-cols-1 md:grid-cols-3 items-center rounded-xl bg-rebin-grey gap-y-10 gap-x-10 p-6">
+    <section className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center rounded-xl bg-rebin-grey gap-y-10 md:gap-x-10 p-6">
       <img
-        className="h-full object-cover rounded-xl"
+        className="mx-auto h-full object-cover rounded-xl"
         src={binMaarkedal}
         alt="Gloednieuwe vuilnisbak in Maarkedal met een sensor van Rebin"
       />
-      <div className="flex flex-col col-span-2 gap-y-5 text-justify">
-        <p className="text-lg text-rebin-darkblue max-w-sm md:max-w-full">
-          {testimonial}
-        </p>
+      <div className="flex flex-col lg:col-span-2 gap-y-5 text-justify">
+        <p className="text-lg text-rebin-darkblue max-w-full">{testimonial}</p>
         <div className="flex gap-x-5 items-center w-fit">
           <img className="h-10 w-auto sm:h-auto" src={joris} alt="" />
           <div className="flex flex-col">
