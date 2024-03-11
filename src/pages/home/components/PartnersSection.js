2e72d3f5ec8360fe143d1bb2c973a7cff83626cd
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-11 11:55:53 +0100

    refactor

--- src/pages/home/components/PartnersSection.js
+++ src/pages/home/components/PartnersSection.js
@@ -18,7 +18,7 @@ const PartnersSection = () => {
       src: unizo,
     },
     {
-      name: "Vlaio",
+      name: "VLAIO",
       src: VLAIO,
     },
     {
@@ -66,24 +66,6 @@ const PartnersSection = () => {
   return (
     <section className="bg-rebin-grey text-center py-8 space-y-6 lg:px-36">
       <p className=" text-slate-500">Trusted by the following partners</p>
-      {/* <div className="grid grid-cols-2 gap-y-6 lg:grid-cols-4 divide-x divide-solid place-content-stretch">
-        <div className="flex justify-center space-x-3">
-          <img src={startItKBC} alt="Logo Start It KBC" className="my-auto" />
-          <p className="my-auto font-medium text-lg">Start it KBC</p>
-        </div>
-        <div className="flex justify-center space-x-3">
-          <img src={unizo} alt="partner" className="my-auto" />
-          <p className="my-auto font-medium text-lg">Unizo</p>
-        </div>
-        <div className="flex justify-center space-x-3">
-          <img src={VLAIO} alt="Logo VLAIO" className="my-auto" />
-          <p className="my-auto font-medium text-lg">Vlaio</p>
-        </div>
-        <div className="flex justify-center space-x-3">
-          <img src={voka} alt="Logo Voka" className="my-auto" />
-          <p className="my-auto font-medium text-lg">Voka</p>
-        </div>
-      </div> */}
       <Slider {...settings}>
         {partners.map((_, index) => (
           <div key={index} className="slider-container">
