c0ff40855df405bda6826067766c2740ccaa9062
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-12 09:13:43 +0100

    update styling statistic bars

--- src/pages/home/components/StatisticBar.js
+++ src/pages/home/components/StatisticBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const StatisticBar = () => {
   return (
     <div className="bg-rebin-grey py-4 rounded-xl">
-      <dl className="grid divide-y-0 grid-cols-2  sm:divide-y-0 md:grid-cols-4 divide-x divide-solid">
+      <dl className="grid divide-y-0 grid-cols-2  sm:divide-y-0 md:grid-cols-4 divide-x divide-solid divide-rebin-darkblue">
         <div className="grid place-content-center text-center space-y-5 p-6">
           <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
             1.221
