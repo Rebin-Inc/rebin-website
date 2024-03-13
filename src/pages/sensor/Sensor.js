62447227f25be6f69a2eebebae0eea76b21f8e30
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-13 15:36:02 +0100

    start implementation of sensor page

--- src/pages/sensor/Sensor.js
+++ src/pages/sensor/Sensor.js
@@ -1,29 +1,45 @@
 import React from "react";
 import StatisticBar from "./components/StatisticBar";
 import ArrowRightWhite from "../../assets/images/Pijl_Rechts_Wit.svg";
+import Sensor1 from "../../assets/images/sensor/sensor1.png";
+import Sensor3 from "../../assets/images/sensor/sensor3.png";
+import SensorMenu from "./components/SensorMenu";
+import SpecificationBar from "./components/SpecificationBar";
+import CallToAction from "../../components/CallToAction";
 
 const Sensor = () => {
   return (
-    <main>
-      <div className="grid grid-cols-2 max-w-7xl mx-auto my-24">
-        <div>
-          <h1 className="font-bold text-4xl">The Rebin smart bin sensor</h1>
-          <h2 className="text-gradient-horizontal font-bold text-3xl">
-            Small. Advanced. Sustainable
+    <main className="space-y-20">
+      <div className="grid md:grid-cols-2 max-w-7xl mx-auto mt-20">
+        <div className="my-auto">
+          <h1 className="font-bold text-5xl">The <span className="text-gradient-horizontal ">Rebin</span> smart bin sensor</h1>
+          <h2 className="mt-4 text-3xl">
+            Small. Advanced. Sustainable.
           </h2>
-          <button className="relative inline-flex items-center justify-center space-x-2 px-5 rounded-full p-2 bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+          <button className="mt-8 relative inline-flex items-center justify-center space-x-2 px-5 rounded-full p-2 bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span className="sr-only">Request sensor</span>
             Request sensor
             <img src={ArrowRightWhite} alt="Rebin Arrow" className="w-8" />
           </button>
         </div>
-        <div className="grid place-content-center">
-          <div className="blob"></div>
-          <img src="" alt="Sensor" />
+        <div className="grid place-items-center mt-6">
+          <div className="blob absolute -z-10"></div>
+          <img src={Sensor1} alt="Sensor" className="w-1/2" />
         </div>
       </div>
       <StatisticBar />
-    </main>
+      <div>
+        <h3 className="text-gradient-horizontal font-bold text-4xl text-center leading-normal">Advantages</h3>
+        <h4 className="font-bold text-4xl mt-2 w-2xl text-center text-rebin-darkblue">Where hardware meets innovation</h4>
+        <SensorMenu></SensorMenu>
+      </div>
+      <div>
+        <h3>Technical specifications</h3>
+        <h4>Get more insights about our sensor</h4>
+        <SpecificationBar></SpecificationBar>
+      </div>
+      <CallToAction></CallToAction>
+    </main >
   );
 };
 
---
+++ src/pages/sensor/components/SensorMenu.js
@@ -0,0 +1,104 @@
+import {
+    Tabs,
+    TabsHeader,
+    TabsBody,
+    Tab,
+    TabPanel,
+} from "@material-tailwind/react";
+import React from "react";
+import Sensor2 from "../../../assets/images/sensor/sensor2.png";
+import parse from "html-react-parser";
+
+export default function SensorMenu() {
+    const [activeTab, setActiveTab] = React.useState(0);
+
+    const data = [
+        {
+            label: "Small",
+            title: "The smallest sensor",
+            desc: `
+            With its minimal volume (<b>the smallest sensor</b> on the European market!) and its <b>unique design</b>, 
+            the Rebin smart bin sensor is compatible in every type of bin and container. 
+            Furthermore, the sensor can be <b>placed at any possible angle</b>.
+            <br />
+            <br />
+            All this ensures that the sensor can be <b>installed in less than a minute</b>, 
+            such that in no time you can experience all the benefits the Repoint platform has to offer. 
+            `,
+        },
+        {
+            label: "Advanced",
+            title: "Advanced features",
+            desc: `
+            Thanks to its <b>accurate laser sensor</b>, precise measurements are guaranteed for any type of content, 
+            ranging from containers with heights of a dozen centimeters to several meters.
+            <br />
+            <br />
+            Through <b>open API access</b>, there is an added possibility 
+            to integrate the sensor in no time with your own personal software package.
+            `,
+        },
+        {
+            label: "Sustainable",
+            title: "Sustainable design",
+            desc: `
+            By choosing for the <b>highest quality, recyclable</b> components and 
+            manufacturing entirely <b>in Belgium</b>, 
+            we guarantee a sensor that is completely <b>shock and water resistant</b>. 
+            In addition, the sensor can withstand a wide range of temperatures.
+            <br />
+            <br />
+            The premium battery guarantees a lifespan of at least <b>5 years</b> and is replaceable. 
+            In this way, unnecessary future costs for sensor purchases are avoided.
+            `,
+        },
+    ];
+
+    return (
+        <Tabs value={activeTab}>
+            <TabsHeader
+                className="mt-20 h-36 divide-x p-0 rounded-lg overflow-hidden"
+                indicatorProps={{
+                    className: "shadow-none mt-1",
+                }}>
+                {data.map(({ label }, index) => (
+                    <button className={activeTab === index ? "w-full border-t-[12px] border-grey rounded-md" : "w-full"}>
+                        <Tab key={index} value={index} className="shadow-md -mt-1" onClick={() => setActiveTab(index)}>
+                            <div className=
+                                {activeTab === index ? "mx-auto bg-gradient w-8 aspect-square rounded-full text-white font-bold text-xl flex place-content-center"
+                                    : "mx-auto bg-rebin-grey w-8 aspect-square rounded-full font-bold text-xl flex place-content-center"}>
+                                <div className={activeTab === index ? "h-min text-white" : "h-min text-gradient-horizontal"}>
+                                    {index + 1}
+                                </div>
+                            </div>
+                            <p className="font-bold text-xl text-rebin-darkblue mt-1">
+                                {label}
+                            </p>
+                        </Tab>
+                    </button>
+                ))}
+            </TabsHeader>
+            <TabsBody
+                animate={{
+                    initial: { y: 250 },
+                    mount: { y: 0 },
+                    unmount: { y: 250 },
+                }}
+            >
+                {data.map(({ title, desc }, index) => (
+                    <TabPanel
+                        key={index}
+                        value={index}
+                        className="grid md:grid-cols-2 gap-8 mt-8"
+                    >
+                        <img className="mx-auto" src={Sensor2} alt="The worlds smallest waste sensor" />
+                        <div className="space-y-4">
+                            <h3 className="text-gradient-horizontal font-bold text-xl">{title}</h3>
+                            <p className="text-black font-medium drop-shadow-xl">{parse(desc)}</p>
+                        </div>
+                    </TabPanel>
+                ))}
+            </TabsBody>
+        </Tabs >
+    );
+}
