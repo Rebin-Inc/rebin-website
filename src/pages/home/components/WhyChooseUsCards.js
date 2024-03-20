5aba4a1db5aa03c88da66d06ed89265e22d51afb
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-20 15:38:55 +0100

    refactor + add images to cards

--- src/pages/home/components/WhyChooseUsCards.js
+++ src/pages/home/components/WhyChooseUsCards.js
@@ -4,8 +4,47 @@ import { Card, CardBody } from "@material-tailwind/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Support from "../../../assets/images/whyChooseUs/support.jpg";
+import Sensor from "../../../assets/images/whyChooseUs/sensor.jpg";
+import Dashboard from "../../../assets/images/whyChooseUs/dashboard.jpg";
 
 const WhyChooseUsCards = () => {
+
+  const data = [
+    {
+      title: "24/7 support",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, animi placeat natus repellendus ducimus",
+      image: Support,
+      rowStart: "row-start-1",
+      colStart: "col-start-1",
+      imagePosition: "right",
+    },
+    {
+      title: "World's best sensor",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, animi placeat natus repellendus ducimus",
+      image: Sensor,
+      rowStart: "row-start-1",
+      colStart: "col-start-5",
+      imagePosition: "left",
+    },
+    {
+      title: "User-friendly",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, animi placeat natus repellendus ducimus",
+      image: Dashboard,
+      rowStart: "row-start-4",
+      colStart: "col-start-1",
+      imagePosition: "right",
+    },
+    {
+      title: "100% Belgium made",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, animi placeat natus repellendus ducimus",
+      image: Support,
+      rowStart: "row-start-4",
+      colStart: "col-start-5",
+      imagePosition: "left",
+    },
+  ]
+
   const settings = {
     dots: true,
     infinite: true,
@@ -26,70 +65,23 @@ const WhyChooseUsCards = () => {
   return (
     <div className="mt-16">
       <div className="hidden xl:grid grid-cols-7 grid-rows-5 bg-dotted-circle bg-center bg-no-repeat bg-contain p-16">
-        <Card shadow={false} className="row-start-1 col-start-1 col-span-3 row-span-2 border shadow-around">
-          <CardBody className="flex h-full">
-            <div className="p-4 pl-0 text-balance space-y-2 w-full">
-              <h4 className="mb-1 text-2xl font-bold">24/7 support</h4>
-              <p className="text-lg text-black">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum,
-                animi placeat natus repellendus ducimus
-              </p>
-            </div>
-            <img
-              src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-              alt=""
-              className="object-cover rounded-xl w-36"
-            />
-          </CardBody>
-        </Card>
-        <Card shadow={false} className="row-start-1 col-start-5 col-span-3 row-span-2 border shadow-around">
-          <CardBody className="flex h-full">
-            <img
-              src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-              alt=""
-              className="object-cover rounded-xl w-36"
-            />
-            <div className="p-4 pr-0 text-balance space-y-2 w-full">
-              <h4 className="mb-1 text-2xl font-bold">World's best sensor</h4>
-              <p className="text-lg text-black">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum,
-                animi placeat natus repellendus ducimus
-              </p>
-            </div>
-          </CardBody>
-        </Card>
-        <Card shadow={false} className="row-start-4 col-start-1 col-span-3 row-span-2 border shadow-around">
-          <CardBody className="flex h-full">
-            <div className="p-4 pl-0 text-balance space-y-2 w-full">
-              <h4 className="mb-1 text-2xl font-bold">User-friendly</h4>
-              <p className="text-lg text-black">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum,
-                animi placeat natus repellendus ducimus
-              </p>
-            </div>
-            <img
-              src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-              alt=""
-              className=" object-cover rounded-xl w-36"
-            />
-          </CardBody>
-        </Card>
-        <Card shadow={false} className="row-start-4 col-start-5 col-span-3 row-span-2 border shadow-around">
-          <CardBody className="flex h-full">
-            <img
-              src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-              alt=""
-              className="object-cover rounded-xl w-36"
-            />
-            <div className="p-4 pr-0 text-balance space-y-2 w-full">
-              <h4 className="mb-1 text-2xl font-bold">100% Belgium made</h4>
-              <p className="text-lg text-black">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum,
-                animi placeat natus repellendus ducimus
-              </p>
-            </div>
-          </CardBody>
-        </Card>
+        {data.map((item, index) => (
+          <Card key={index} shadow={false} className={`${item.colStart} ${item.rowStart} col-span-3 row-span-2 border shadow-around`}>
+            <CardBody className="flex h-full items-center gap-4">
+              <div className="text-balance space-y-2 w-full">
+                <h4 className="mb-1 text-2xl font-bold">{item.title}</h4>
+                <p className="text-lg text-black">
+                  {item.description}
+                </p>
+              </div>
+              <img
+                src={item.image}
+                alt=""
+                className={`object-cover rounded-xl w-36 ${item.imagePosition === "right" ? "order-last" : "order-first"}`}
+              />
+            </CardBody>
+          </Card>
+        ))}
         <Card className="row-start-3 col-start-4 w-fit h-fit m-auto">
           <img src={RebinRBlue} alt="" className="w-24 aspect-square" />
         </Card>
@@ -99,72 +91,24 @@ const WhyChooseUsCards = () => {
           <img src={RebinRBlue} alt="" className="w-24 aspect-square" />
         </Card>
         <Slider {...settings} className="shadow-around rounded-lg max-h-30">
-          <Card className="border">
-            <CardBody className="flex h-64">
-              <div className="pr-4 md:p-4 text-balance space-y-2 w-full">
-                <h4 className="mb-1 text-2xl font-bold">24/7 support</h4>
-                <p className="text-lg text-black line-clamp-4">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Rerum, animi placeat natus repellendus ducimus
-                </p>
-              </div>
-              <img
-                src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-                alt=""
-                className="object-cover rounded-xl w-36"
-              />
-            </CardBody>
-          </Card>
-          <Card className="border">
-            <CardBody className="flex h-64">
-
-              <div className="pr-4 md:p-4 text-balance space-y-2 w-full">
-                <h4 className="mb-1 text-2xl font-bold">World's best sensor</h4>
-                <p className="text-lg text-black line-clamp-4">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Rerum, animi placeat natus repellendus ducimus
-                </p>
-              </div>
-              <img
-                src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-                alt=""
-                className="object-cover rounded-xl w-36"
-              />
-            </CardBody>
-          </Card>
-          <Card className="border h-64">
-            <CardBody className="flex h-full">
-              <div className="pr-4 md:p-4 text-balance space-y-2 w-full">
-                <h4 className="mb-1 text-2xl font-bold">User-friendly</h4>
-                <p className="text-lg text-black line-clamp-4">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Rerum, animi placeat natus repellendus ducimus
-                </p>
-              </div>
-              <img
-                src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-                alt=""
-                className="object-cover rounded-xl w-36"
-              />
-            </CardBody>
-          </Card>
-          <Card className="border">
-            <CardBody className="flex h-64">
-
-              <div className="pr-4 md:p-4 text-balance space-y-2 w-full">
-                <h4 className="mb-1 text-2xl font-bold">100% Belgium made</h4>
-                <p className="text-lg text-black line-clamp-4">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Rerum, animi placeat natus repellendus ducimus
-                </p>
-              </div>
-              <img
-                src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80"
-                alt=""
-                className="object-cover rounded-xl w-36"
-              />
-            </CardBody>
-          </Card>
+          {
+            data.map((item, index) => (
+              <Card key={index} className="border">
+                <CardBody className="flex h-64">
+                  <div className="pr-4 md:p-4 text-balance space-y-2 w-full">
+                    <h4 className="mb-1 text-2xl font-bold">{item.title}</h4>
+                    <p className="text-lg text-black line-clamp-4">
+                      {item.description}
+                    </p>
+                  </div>
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="object-cover rounded-xl w-36"
+                  />
+                </CardBody>
+              </Card>
+            ))}
         </Slider>
       </div>
     </div>
