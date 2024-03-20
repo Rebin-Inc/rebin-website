8c94acfbb8366f6d6854e11cb08a167a1fea2016
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-20 11:27:29 +0100

    Open clicked links at top of page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
