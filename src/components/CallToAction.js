c65a33ba56ae72bc89002fc5864099c95fa192e0
Senne Malfliet <senne.malfliet@rebin.be>
2024-03-25 11:47:59 +0100

    add background

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Button from "../components/Button";
+import CtaBackground from "../assets/images/callToAction/cta_bg.svg";
+import LeftArrow from "../assets/images/callToAction/left.svg";
+import RightArrow from "../assets/images/callToAction/right.svg";
 
 const CallToAction = () => {
   return (
-    <section className="text-center -mt-20 h-80">
-      <div className="absolute h-80 w-full min-w-max left-0 -z-10">
+    <section className="-mt-20 h-64 sm:h-80 md:h-96 grid">
+      <div className="absolute h-64 sm:h-80 md:h-96 w-full min-w-max left-0 -z-10">
+        <div className="flex gap-x-12 w-full h-full">
+          <img src={LeftArrow} alt="" className="mr-auto h-full" />
+          <img src={RightArrow} alt="" className="ml-auto h-full invisible sm:visible" />
+        </div>
       </div>
-      <div className="text-center space-y-6 pt-24">
+      <div className="space-y-6 place-self-center">
         <h2 className="text-3xl font-bold">
           Is your organization next?
         </h2>
-        <Button text="Contact us" arrow={true} href="/contact" />
+        <h3 className="text-3xl">Start now</h3>
+        <div className="mx-auto w-fit">
+          <Button text="Contact us" arrow={true} href="/contact" margin="0" />
+        </div>
       </div>
     </section>
   )
