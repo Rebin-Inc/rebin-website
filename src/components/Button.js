36de175f871b382216b6c5afbfa435d71fa26bda
Senne Malfliet <senne.malfliet@gmail.com>
2024-03-26 10:57:47 +0100

    tweak button animation

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import ArrowRightWhite from "../assets/images/Pijl_Rechts_Wit.svg"
 
 const Button = ({ text, width, arrow, href, margin }) => {
     return (
-        <button className={`w-${width ? width : "fit"} ${margin ? margin : "mt-10"} p-0 rounded-lg bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white transform active:scale-90 transition-transform`}>
+        <button className={`w-${width ? width : "fit"} ${margin ? margin : "mt-10"} p-0 rounded-lg bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white transform hover:scale-95 transition-transform active:scale-90`}>
             <Link to={href} className="w-full p-3 flex justify-center items-center">
                 <span className="sr-only">{text}</span>
                 {text}
