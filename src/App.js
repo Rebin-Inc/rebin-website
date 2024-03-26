f51a38cc9443fc830df5595ba6a0392464d173b5
Emilio Van Der Linden <emilio.vanderlinden@rebin.be>
2024-03-26 22:21:28 +0100

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from "./Layout";
 import Contact from "./pages/contact/Contact";
 import HowItWorks from "./pages/how-it-works/HowItWorks";
@@ -12,18 +12,20 @@ import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index path="/" element={<Home />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="how-it-works" element={<HowItWorks />} />
-        <Route path="references" element={<References />} />
-        <Route path="about" element={<About />} />
-        <Route path="software" element={<Software />} />
-        <Route path="consulting" element={<Consulting />} />
-        <Route path="sensor" element={<Sensor />} />
-      </Route>
-    </Routes>
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="how-it-works" element={<HowItWorks />} />
+          <Route path="references" element={<References />} />
+          <Route path="about" element={<About />} />
+          <Route path="software" element={<Software />} />
+          <Route path="consulting" element={<Consulting />} />
+          <Route path="sensor" element={<Sensor />} />
+        </Route>
+      </Routes>
+    </Router> 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
