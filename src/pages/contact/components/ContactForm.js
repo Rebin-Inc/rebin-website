80301618db429ff4277dc5016a6cde549e11a291
Emilio Van Der Linden <emilio.vanderlinden@rebin.be>
2024-03-26 10:34:21 +0100

    Contact form fix

--- src/pages/contact/components/ContactForm.js
+++ src/pages/contact/components/ContactForm.js
@@ -92,7 +92,7 @@ const ContactForm = () => {
                 ></textarea>
               </div>
 
-              <button className="mx-auto sm:ml-auto w-fit p-3 flex justify-center items-center rounded-lg bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <button className="mx-auto sm:mr-0 w-fit p-3 flex justify-center items-center rounded-lg bg-gradient font-bold text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="sr-only">Send message</span>
                 Send Message
               </button>
