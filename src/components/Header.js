ab9db6d065863470db4719c7b8e163ab8a91b063
Senne Malfliet <senne.malfliet@gmail.com>
2024-03-26 17:03:43 +0100

    fix link to login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -227,7 +227,7 @@ const Header = () => {
                 </div>
                 <div className="py-6">
                   <Link
-                    to="login"
+                    to="https://rebin-prod.we-are.be/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-medium leading-7 text-gray-900 hover:bg-gray-50"
                     onClick={() => setMobileMenuOpen(false)}
                   >
