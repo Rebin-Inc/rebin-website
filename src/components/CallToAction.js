ede0bb6570ffd641c50202f4382cc5339151cb62
Senne Malfliet <senne.malfliet@gmail.com>
2024-03-26 10:27:12 +0100

    fix horizontal scroll issue

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,7 +9,7 @@ const CallToAction = () => {
       <div className="absolute h-80 sm:h-70 md:h-96 w-full min-w-max left-0 -z-10">
         <div className="flex gap-x-6 w-full h-80 xs:h-56 md:h-full">
           <img src={LeftArrow} alt="" className="mr-auto h-full" />
-          <img src={RightArrow} alt="" className="ml-auto h-full hidden sm:block" />
+          <img src={RightArrow} alt="" className="ml-auto h-full hidden md:block" />
         </div>
       </div>
       <div className="space-y-6 place-self-center">
