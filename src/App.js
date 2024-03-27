bc6ccdab0cb2f2c300edb142abfadc376701de8c
Senne Malfliet <senne.malfliet@gmail.com>
2024-03-27 11:08:55 +0100

    fix url of homepage

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route  element={<Layout />}>
+        <Route element={<Layout />}>
           <Route index path="/" element={<Home />} />
           <Route path="contact" element={<Contact />} />
           <Route path="how-it-works" element={<HowItWorks />} />
