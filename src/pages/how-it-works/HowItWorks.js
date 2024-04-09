eb626844fb7f694454cff3a6074c6fa8c45abe3f
Senne Malfliet <senne.malfliet@gmail.com>
2024-04-09 11:47:26 +0200

    split stats into multiple svg's to improve loading times

--- src/pages/how-it-works/HowItWorks.js
+++ src/pages/how-it-works/HowItWorks.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import AdvantageBar from './components/AdvantageBar';
 import CallToAction from '../../components/CallToAction';
-import Stats from '../../assets/images/howItWorks/stats.svg';
+import BinLevel from '../../assets/images/howItWorks/bin_level.svg';
+import BinsCollected from '../../assets/images/howItWorks/bins_collected.svg';
+import Co2 from '../../assets/images/howItWorks/co2.svg';
 import FourStepsMenu from './components/FourStepsMenu';
 import Button from '../../components/Button';
 
@@ -22,11 +24,16 @@ const HowItWorks = () => {
           </p>
           <Button text="Book a demo" href="/contact" />
         </div>
-        <div className="mx-auto max-w-lg place-self-center">
-          <img
-            src={Stats}
-            alt="Infographic representing the Repoint platform"
-          ></img>
+        <div className="mx-auto mt-6 grid w-fit space-y-8 lg:mt-0 lg:max-w-md xl:max-w-lg xl:space-y-12">
+          <img src={BinLevel} alt="" className="rounded-xl shadow-around" />
+          <div className="flex justify-between">
+            <img
+              src={BinsCollected}
+              alt=""
+              className="w-4/12 rounded-xl shadow-around "
+            />
+            <img src={Co2} alt="" className="w-7/12 rounded-xl shadow-around" />
+          </div>
         </div>
       </div>
       <AdvantageBar></AdvantageBar>
